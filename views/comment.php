abd862f80cf660a94ad86bbb3ccdf8017ae753ac
dufva <sussiedufva@hotmail.com>
2021-03-08 14:27:39 +0100

    comments fully fixed

--- views/comment.php
+++ views/comment.php
@@ -15,49 +15,44 @@
      
     $stmt = $pdo->prepare("SELECT id, title, message FROM posts WHERE id=:id_IN");
     $stmt->bindParam(":id_IN", $_GET['id']);
- 
+
     $success = $stmt->execute();
- 
-    if(!$success){
-         echo "<h3>Något gick fel</h3>";
-         die();
+
+    while($message = $stmt->fetch()){
+        echo $message['title'] . "<br/>" . $message['message'] . "<br/>";
     }
-     
-    $userData = $stmt->fetch();
     
     //koden ovan behövs för att hämta meddelandet jag vill kommentera
 ?>
-<p>
-    <?=$userData['message']?> <!-- skriver ut meddelandet på sidan -->
-</p>
+
 <h3>Här kan du kommentera inlägget: </h3>
     <form method="post" action="comment.php?action=comment">
         <input type="hidden" name="id" value="<?=$_GET['id']?>">
-        <b><?=$_SESSION['username']?> </b> </br>
+        <input type="hidden" name="username" value="<?=$_SESSION['username']?>"> </br>
         <label for="text">Kommentera: </label>
         <input type="text" name="comment">
         <input type="submit" value="Kommentera">
-    </form>
+    </form> 
     
 <?php
 
     $action = (isset($_GET['action']) ? $_GET['action'] : "");
     
     if(isset($action) && $action == "comment"){
-        $sql = "INSERT INTO comments (content, user, postsid) VALUES(:comment_IN, :user_IN, :id_IN)";
+        $sql = "INSERT INTO comments (content, user, postsid) VALUES(:comment_IN, :username_IN, :id_IN)";
         $stmt = $pdo->prepare($sql);
         $stmt->bindParam(':comment_IN', $_POST['comment']);
-        $stmt->bindParam(':user_IN', $_SESSION['username']);
+        $stmt->bindParam(':username_IN', $_SESSION['username']);
         $stmt->bindParam(':id_IN', $_POST['id']); //kopplar foreign key postsid
     
     if($stmt->execute()) {
-        header("location:comment.php");
+        header("location:comment.php?id=".$_POST['id']);
     } 
 }
 
-    $stm = $pdo->query("SELECT * FROM comments WHERE postsid=".$_GET['id']);
+    $comment_stm = $pdo->query("SELECT id, content FROM comments WHERE postsid=".$_GET['id']);
 
-    while($row = $stm->fetch()){
+    while($row = $comment_stm->fetch()){
         echo "<p>";
         echo "<a href=\"editPosts.php?id=". $row['id'] . "\">"  .$row['id']. "</a>" . ". " . $row['content'] . "<br />" ;
         echo "</p>";
